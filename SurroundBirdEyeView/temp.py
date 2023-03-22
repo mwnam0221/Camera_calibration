81e0cdbae9e8c765b83ce4031f76926932034a8d
mwn <mwn0221@deltax.ai>
2023-03-23 00:39:08 +0900

    first commit

---
+++ SurroundBirdEyeView/temp.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+
+
+back_D=np.load('C:/Users/ok/Desktop/CameraCalibration/SurroundBirdEyeView/data/back/camera_back_D.npy') #distortion matrix
+back_K=np.load('C:/Users/ok/Desktop/CameraCalibration/SurroundBirdEyeView/data/back/camera_back_K.npy') #camera matrix
+back_H=np.load('C:/Users/ok/Desktop/CameraCalibration/SurroundBirdEyeView/data/back/camera_back_H.npy') #camera matrix
+
